371a00cc47250579058c8cb9b241b237dd21ae36
SOFT-cRONOS <103128033+SOFT-cRONOS@users.noreply.github.com>
2023-08-03 17:34:31 -0300

    bd

--- Admin/CrearBD.sql
+++ Admin/CrearBD.sql
@@ -1,4 +1,4 @@
-CREATE TABLE usuarios (
+CREATE TABLE usuario (
     idusuario INT AUTO_INCREMENT PRIMARY KEY,
     idrol INT,
     nick VARCHAR(20),
@@ -20,7 +20,7 @@ CREATE TABLE rol (
 
 CREATE TABLE permisos (
     idpermiso INT AUTO_INCREMENT PRIMARY KEY,
-    nombre INT,
+    nombre VARCHAR(20),
     detalle VARCHAR(255)
 )
 
@@ -43,11 +43,73 @@ CREATE TABLE articulo (
     descripcion VARCHAR(255),
     imagen VARCHAR(20),
     estado BIT,
-    compuesto BOOLEAN
+    compuesto BOOLEAN,
+    FOREIGN KEY (idcategoria) REFERENCES categoria(idcategoria)
 )
 
-CREATE TABLE asticulos_compuestos (
+CREATE TABLE articulos_compuestos (
     idarticulo INT KEY,
     idcomponente INT,
-    cantidad INT
+    cantidad INT,
+    FOREIGN KEY (idarticulo) REFERENCES articulo(idarticulo),
+    FOREIGN KEY (idcomponente) REFERENCES articulo(idarticulo)
 )
+
+CREATE TABLE variante_producto (
+    id_variante INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    codigo INT,
+    nombre varchar(100),
+    precio_venta DECIMAL(11,2),
+    precio_compra DECIMAL(11,2),
+    ganancia INT,
+    stock INT,
+    descripcion VARCHAR(255),
+    imagen VARCHAR(20),
+    estado BIT
+    FOREIGN KEY (id_producto) REFERENCES articulo(idarticulo)
+)
+
+CREATE TABLE categoria (
+    idcategoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    detalle VARCHAR(255),
+    estado BIT
+)
+
+CREATE TABLE proveedor (
+    idproveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    direccion VARCHAR(100),
+    telefono VARCHAR(100),
+    empresa VARCHAR(100),
+    web VARCHAR(100)
+)
+
+CREATE TABLE ingreso (
+    idingreso INT AUTO_INCREMENT PRIMARY KEY,
+    idproveedor INT KEY,
+    idusuario INT,
+    tipo_comprobante VARCHAR(20),
+    serie_comprobante VARCHAR(7),
+    num_comprobante VARCHAR(10),
+    fecha DATE,
+    impuesto DECIMAL(4,2),
+    total DECIMAL(11,2),
+    estado VARCHAR(20),
+    FOREIGN KEY (idproveedor) REFERENCES proveedor(idproveedor),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario)
+)
+
+CREATE TABLE detalle_ingreso (
+    iddetalle_ingreso INT AUTO_INCREMENT PRIMARY KEY,
+    idingreso INT KEY,
+    cantidad INT,
+    idarticulo INT,
+    link VARCHAR(255),
+    precio DECIMAL(11,2),
+    FOREIGN KEY (idingreso) REFERENCES ingreso(idingreso),
+    FOREIGN KEY (idarticulo) REFERENCES articulo(idarticulo)
+)
+
+

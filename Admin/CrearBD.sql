9b5440df6ccb916e263c4c3444b6a7489a45ff8e
SOFT-cRONOS <103128033+SOFT-cRONOS@users.noreply.github.com>
2023-08-04 10:49:57 -0300

    bd

--- Admin/CrearBD.sql
+++ Admin/CrearBD.sql
@@ -14,10 +14,10 @@ CREATE TABLE usuario (
     contrasena VARCHAR(20),
     estado VARCHAR(10),
     FOREIGN KEY (idrol) REFERENCES rol(idrol)
-)
+);
 
 INSERT INTO usuario (idrol, nick, documento, nombre, direccion, telefono, email, contrasena, estado)
-VALUES (1, 'admin', '1234', 'Administrador','Dirección', 'hermespos', 'usuario1@example.com', 'hermespos', 'activo'),
+VALUES (1, 'admin', '1234', 'Administrador','Dirección', 'hermespos', 'usuario1@example.com', 'hermespos', 'activo');
 
 
 
@@ -25,19 +25,19 @@ CREATE TABLE rol (
     idrol INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(10),
     detalle VARCHAR(255)
-)
+);
 
 INSERT INTO rol (nombre, detalle)
-VALUES ('admin', 'tiene control total sobre la aplicacion y los datos'),
+VALUES ('admin', 'tiene control total sobre la aplicacion y los datos');
 
 CREATE TABLE permisos (
     idpermiso INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(20),
     detalle VARCHAR(255)
-)
+);
 
 INSERT INTO permisos (nombre, detalle)
-VALUES ('editar', 'Ver y agregar nuevos productos y clientes')
+VALUES ('editar', 'Ver y agregar nuevos productos y clientes');
 
 
 CREATE TABLE asig_permisos (
@@ -46,10 +46,10 @@ CREATE TABLE asig_permisos (
     INDEX idrol_index (idrol),
     FOREIGN KEY (idrol) REFERENCES rol(idrol) ON DELETE CASCADE,
     FOREIGN KEY (idpermiso) REFERENCES permisos(idpermiso)
-)
+);
 
 INSERT TABLE asig_permisos (idrol, idpermiso)
-VALUES (1,1),
+VALUES (1,1);
 
 CREATE TABLE articulo (
     idarticulo INT AUTO_INCREMENT PRIMARY KEY,
@@ -65,7 +65,7 @@ CREATE TABLE articulo (
     estado BIT,
     compuesto BOOLEAN,
     FOREIGN KEY (idcategoria) REFERENCES categoria(idcategoria)
-)
+);
 
 CREATE TABLE articulos_compuestos (
     idarticulo INT KEY,
@@ -73,7 +73,7 @@ CREATE TABLE articulos_compuestos (
     cantidad INT,
     FOREIGN KEY (idarticulo) REFERENCES articulo(idarticulo),
     FOREIGN KEY (idcomponente) REFERENCES articulo(idarticulo)
-)
+);
 
 CREATE TABLE variante_producto (
     id_variante INT AUTO_INCREMENT PRIMARY KEY,
@@ -88,14 +88,14 @@ CREATE TABLE variante_producto (
     imagen VARCHAR(20),
     estado BIT
     FOREIGN KEY (id_producto) REFERENCES articulo(idarticulo)
-)
+);
 
 CREATE TABLE categoria (
     idcategoria INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50),
     detalle VARCHAR(255),
     estado BIT
-)
+);
 
 CREATE TABLE proveedor (
     idproveedor INT AUTO_INCREMENT PRIMARY KEY,
@@ -104,7 +104,7 @@ CREATE TABLE proveedor (
     telefono VARCHAR(100),
     empresa VARCHAR(100),
     web VARCHAR(100)
-)
+);
 
 CREATE TABLE ingreso (
     idingreso INT AUTO_INCREMENT PRIMARY KEY,
@@ -119,7 +119,7 @@ CREATE TABLE ingreso (
     estado VARCHAR(20),
     FOREIGN KEY (idproveedor) REFERENCES proveedor(idproveedor),
     FOREIGN KEY (idusuario) REFERENCES usuario(idusuario)
-)
+);
 
 CREATE TABLE detalle_ingreso (
     iddetalle_ingreso INT AUTO_INCREMENT PRIMARY KEY,
@@ -130,7 +130,7 @@ CREATE TABLE detalle_ingreso (
     precio DECIMAL(11,2),
     FOREIGN KEY (idingreso) REFERENCES ingreso(idingreso),
     FOREIGN KEY (idarticulo) REFERENCES articulo(idarticulo)
-)
+);
 
 CREATE TABLE cliente (
     idcliente INT AUTO_INCREMENT PRIMARY KEY,
@@ -140,17 +140,37 @@ CREATE TABLE cliente (
     telefono VARCHAR(20),
     email VARCHAR(50) UNIQUE,
     empresa VARCHAR(30)
-)
+);
 
 CREATE TABLE tipo_pago (
     idtipopago INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(10),
     detalle VARCHAR(50)
-)
+);
 
 CREATE TABLE transaccion (
     idtransaccion INT AUTO_INCREMENT PRIMARY KEY,
     idcliente INT,
     tipo_comprobante VARCHAR(20),
-    
+    serie_comprobante VARCHAR(20),
+    fecha_venta DATE,
+    fecha_pedido DATE,
+    idimpuesto INT,
+    iddescuentoa INT,
+    descuento INT,
+    pago DECIMAL(11,2),
+    total DECIMAL(11,2),
+    idtipo_pago INT,
+    estado VARCHAR(20),
+    estado_pago VARCHAR(20)
+    FOREIGN KEY (idtipo_pago) REFERENCES tipo_pago(idtipopago)
+);
+
+CREATE TABLE pagos (
+    idpago INT AUTO_INCREMENT PRIMARY KEY,
+    idtransaccion INT,
+    tipo_pago VARCHAR(20),
+    monto DECIMAL(11, 2),
+    fecha_pago DATE,
+    FOREIGN KEY (idtransaccion) REFERENCES transaccion(idtransaccion)
 )
